d0607bf11841fc81aa6f0bfef4e45717f16d46dd
Ankur Sarkar <sarkarrohonankur@gmail.com>
2024-03-17 20:17:03 +0530

    update

--- app/goal/[id]/page.js
+++ app/goal/[id]/page.js
@@ -1,11 +1,14 @@
 "use client";
 
 import { ThreeDCardDemo } from "@/components/ThreeDCardDemo";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import React, { useEffect, useState } from "react";
 
 const Page = ({ params }) => {
   const [goalId, setGoalId] = useState();
   const [goal, setGoal] = useState(null);
+  const [addAmount, setAddAmount] = useState(0);
 
   useEffect(() => {
     setGoalId(params.id);
@@ -24,7 +27,26 @@ const Page = ({ params }) => {
   }
 
   return (
-    <div>
+    <div className="flex flex-row justify-center items-center text-center m-auto gap-8">
+      <div className="flex flex-col gap-4 outline outline-1 outline-gray-500 p-4 rounded-md">
+        <label
+          className="text-sm font-medium text-black dark:text-white leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+          htmlFor="price"
+        >
+          Price
+        </label>
+        <div className="p-[2px] rounded-lg transition duration-300 group/input flex flex-col gap-4">
+          <input
+            className="flex h-10 w-full border-none bg-gray-50 dark:bg-zinc-800 text-black dark:text-white shadow-input rounded-md px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-neutral-400 dark:placeholder-text-neutral-600 focus-visible:outline-none focus-visible:ring-[2px] focus-visible:ring-neutral-400 dark:focus-visible:ring-neutral-600 disabled:cursor-not-allowed disabled:opacity-50 dark:shadow-[0px_0px_1px_1px_var(--neutral-700)] group-hover/input:shadow-none transition duration-400"
+            id="price"
+            type="number"
+            value={addAmount}
+            onChange={(e) => setAddAmount(e.target.value)}
+            placeholder="Add Amount Saved"
+          />
+          <Button>Add</Button>
+        </div>
+      </div>
       <ThreeDCardDemo goal={goal} />
     </div>
   );
--- components/ThreeDCardDemo.jsx
+++ components/ThreeDCardDemo.jsx
@@ -7,7 +7,7 @@ import Link from "next/link";
 
 export function ThreeDCardDemo({ goal }) {
     return (
-        <CardContainer className="inter-var">
+        <CardContainer className="inter-var text-center">
             <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
                 <CardItem
                     translateZ="50"
@@ -31,7 +31,20 @@ export function ThreeDCardDemo({ goal }) {
                         alt="thumbnail"
                     />
                 </CardItem>
-                <div className="flex justify-between items-center mt-20">
+                <CardItem
+                    translateZ="50"
+                    className="text-xl font-bold text-neutral-600 dark:text-white"
+                >
+                    By {goal.date}
+                </CardItem>
+                <CardItem
+                    as="p"
+                    translateZ="60"
+                    className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
+                >
+                    Remaining Goal : ₹{goal.price}
+                </CardItem>
+                <div className="flex justify-between items-center mt-2">
                     <CardItem
                         translateZ={20}
                         as={Link}
@@ -39,7 +52,7 @@ export function ThreeDCardDemo({ goal }) {
                         target="__blank"
                         className="px-4 py-2 rounded-xl text-xs font-normal w-full outline outline-gray-400 dark:text-white text-center"
                     >
-                        Try now →
+                        Search now →
                     </CardItem>
                 </div>
             </CardBody>
