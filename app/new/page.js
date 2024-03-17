df64c71411813426a03d6cffb9a3cda2dabc723c
Ankur Sarkar <sarkarrohonankur@gmail.com>
2024-03-17 05:55:17 +0530

    create page

--- app/new/page.js
+++ app/new/page.js
@@ -20,7 +20,10 @@ import {
 } from "@/components/ui/select";
 const page = () => {
   const [date, setDate] = React.useState();
-  function DatePickerWithPresets() {
+  const [name, setName] = React.useState("");
+  const [price, setPrice] = React.useState("");
+
+  const DatePickerWithPresets = () => {
     return (
       <Popover>
         <PopoverTrigger asChild>
@@ -57,7 +60,19 @@ const page = () => {
         </PopoverContent>
       </Popover>
     );
-  }
+  };
+
+  const handleSaveButton = () => {
+    console.log(
+      `Name: ${name}, Price: ${price}, Date: ${
+        date ? format(date, "PPP") : "No date selected"
+      }`
+    );
+    const product = { name, price, date: date ? format(date, "PPP") : null };
+    const allGoals = JSON.parse(localStorage.getItem("allGoals") || "[]");
+    allGoals.push(product);
+    localStorage.setItem("allGoals", JSON.stringify(allGoals));
+  };
 
   return (
     <div className="mt-8">
@@ -81,6 +96,9 @@ const page = () => {
                 className="flex h-10 w-full border-none bg-gray-50 dark:bg-zinc-800 text-black dark:text-white shadow-input rounded-md px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-neutral-400 dark:placeholder-text-neutral-600 focus-visible:outline-none focus-visible:ring-[2px] focus-visible:ring-neutral-400 dark:focus-visible:ring-neutral-600 disabled:cursor-not-allowed disabled:opacity-50 dark:shadow-[0px_0px_1px_1px_var(--neutral-700)] group-hover/input:shadow-none transition duration-400"
                 id="name"
                 type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                placeholder="Name of Product"
               />
             </div>
             <label
@@ -94,6 +112,9 @@ const page = () => {
                 className="flex h-10 w-full border-none bg-gray-50 dark:bg-zinc-800 text-black dark:text-white shadow-input rounded-md px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-neutral-400 dark:placeholder-text-neutral-600 focus-visible:outline-none focus-visible:ring-[2px] focus-visible:ring-neutral-400 dark:focus-visible:ring-neutral-600 disabled:cursor-not-allowed disabled:opacity-50 dark:shadow-[0px_0px_1px_1px_var(--neutral-700)] group-hover/input:shadow-none transition duration-400"
                 id="price"
                 type="number"
+                value={price}
+                onChange={(e) => setPrice(e.target.value)}
+                placeholder="Price"
               />
             </div>
             <label
@@ -106,7 +127,10 @@ const page = () => {
               <DatePickerWithPresets />
             </div>
           </div>
-          <Button className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]">
+          <Button
+            onClick={handleSaveButton}
+            className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
+          >
             Save
             <span className="group-hover/btn:opacity-100 block transition duration-500 opacity-0 absolute h-px w-full -bottom-px inset-x-0 bg-gradient-to-r from-transparent via-cyan-500 to-transparent"></span>
             <span className="group-hover/btn:opacity-100 blur-sm block transition duration-500 opacity-0 absolute h-px w-1/2 mx-auto -bottom-px inset-x-10 bg-gradient-to-r from-transparent via-indigo-500 to-transparent"></span>
